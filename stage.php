5fd2216aa512119770b45fbcfedc03746910caa1
ChemicalStan <chemicalstan15@gmail.com>
2018-04-13 22:41:43 +0100

    stage 1 html

---
+++ stage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>stage 1 task</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container"> 
+        <nav class="navbar">
+         <div class="group">
+          <a href="#" id="home">HOME</a>
+          <a href="#" id="about">ABOUT</a>
+          <a href="#" id="contact">CONTACT</a>
+        </div>
+        </nav>
+        <div class="frame">
+        <div class="star">*</div>
+        <p class="text">
+        Hotels.ng Internship vol.4
+        </p>
+        </div>
+<p class="writeup">DATE: <?php echo date('d-m-y h:ma');?> </p>
+    </div>
+</body>
+</html>
